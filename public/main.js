5c30c54b92817161d9ca8a89285af4ae76135016
ashwinramaswamy92 <ashwinramaswamy92@outlook.com>
2025-02-25 13:02:26 +0530

    Update main.js

--- public/main.js
+++ public/main.js
@@ -210,7 +210,7 @@ document.addEventListener('DOMContentLoaded', function () {
         mainData.datasets.forEach(dataset => {
             dataset.data.forEach(point => {
                 worksheet_data.addRow({
-                    labels: dataset.labels, x: point.x, y: point.y
+                    labels: dataset.label, x: point.x, y: point.y
                 });
             }); 
         });
