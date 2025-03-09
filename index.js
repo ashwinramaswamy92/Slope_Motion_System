580cecda5a5d155593762c72cfca93cea93ae66a
Murali47k <murali47karthick@gmail.com>
2025-03-09 11:55:17 +0530

    changed start page

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var userData = {};
 
 // Route to serve login.html as the landing (starting) page
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/login.html");
+  res.sendFile(__dirname + "/public/home.html");
 });
 
 app.use(express.static("public"));
